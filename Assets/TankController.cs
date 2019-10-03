88d8b100a858c2471b73319c5cec3025990c820a
SanteriSuomi <sant.suomi@gmail.com>
2019-10-03 15:05:51 +0300

    Tank controls WIP

---
+++ Assets/TankController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    [SerializeField]
+    private float moveSpeed = 10;
+    [SerializeField]
+    private float rotationSpeed = 1;
+
+    private float verticalMove;
+    private float horizontalMove;
+
+    private bool pressingE;
+    private bool pressingQ;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        verticalMove = Input.GetAxis("Vertical") * moveSpeed;
+        horizontalMove = Input.GetAxis("Horizontal") * moveSpeed;
+
+        pressingE = Input.GetKey(KeyCode.E);
+        pressingQ = Input.GetKey(KeyCode.Q);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.AddRelativeForce(new Vector3(horizontalMove, 0, verticalMove));
+       // rigidBody.AddForce(new Vector3(horizontalMove, 0, verticalMove));
+
+        if (pressingE)
+        {
+            transform.Rotate(new Vector3(0, rotationSpeed, 0), Space.Self);
+        }
+        else if (pressingQ)
+        {
+            transform.Rotate(new Vector3(0, -rotationSpeed, 0), Space.Self);
+        }
+    }
+}
---
+++ Assets/TankTurretController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class TankTurretController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject turretBarrel;
+
+    [SerializeField] [Range(0, 1.0f)]
+    private float turretRotationSpeed = 0.6f;
+    private float mouseHorizontal;
+    private float mouseHorizontalClamped;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+         mouseHorizontal += Input.GetAxis("Mouse X");
+         mouseHorizontalClamped = Mathf.Clamp(mouseHorizontal, -150, 150);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.localRotation = Quaternion.Slerp(Quaternion.identity, Quaternion.Euler(0, mouseHorizontalClamped, 0), turretRotationSpeed);
+    }
+}
