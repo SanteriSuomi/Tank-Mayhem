fd768087e9f83697a6b058cd26d89c7e58edcb4d
Santeri Suomi <sant.suomi@gmail.com>
2019-10-14 01:45:22 +0300

    Small changes to lightmaps, cursor manager etc

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -5,6 +5,6 @@ public class CursorManager : MonoBehaviour
     private void Awake()
     {
         // Lock cursor to the center of the game.
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 }
