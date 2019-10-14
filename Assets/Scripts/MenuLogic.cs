be9d51471e916fe2e6393fff67e72b19dd723348
Santeri Suomi <sant.suomi@gmail.com>
2019-10-14 22:56:12 +0300

    Small changes, main menu wip

---
+++ Assets/Scripts/MenuLogic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLogic : MonoBehaviour
+{
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(0);
+    }
+}
