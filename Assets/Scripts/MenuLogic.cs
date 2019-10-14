abd0b851beb9d4e470e60cbe164d8d6339777cd9
Santeri Suomi <sant.suomi@gmail.com>
2019-10-15 00:15:45 +0300

    Menu quality settings vip

--- Assets/Scripts/MenuLogic.cs
+++ Assets/Scripts/MenuLogic.cs
@@ -1,8 +1,61 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MenuLogic : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject menuButtonsParent = default;
+    [SerializeField]
+    private GameObject optionsButtonsParent = default;
+    [SerializeField]
+    private Slider volumeSlider = default;
+    [SerializeField]
+    private Slider qualitySlider = default;
+    [SerializeField]
+    private TextMeshProUGUI qualityNameText = default;
+
+    private string[] qualitySettings;
+
+    private void Awake()
+    {
+        qualitySettings = QualitySettings.names;
+
+        volumeSlider.maxValue = AudioListener.volume;
+        volumeSlider.value = AudioListener.volume / 2;
+
+        QualitySettings.SetQualityLevel(qualitySettings.Length - 1);
+        qualitySlider.maxValue = qualitySettings.Length - 1;
+        qualitySlider.value = QualitySettings.GetQualityLevel();
+    }
+
+    private void Update()
+    {
+        VolumeSlider();
+        QualitySlider();
+    }
+    
+    private void VolumeSlider()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+
+    private void QualitySlider()
+    {
+        try
+        {
+            QualitySettings.SetQualityLevel((int)qualitySlider.value);
+            qualityNameText.text = qualitySettings[(int)qualitySlider.value];
+        }
+        catch (System.Exception e)
+        {
+            #if UNITY_EDITOR
+            Debug.Log(e);
+            #endif
+        }
+    }
+
     public void LoadScene(string scene)
     {
         SceneManager.LoadScene(scene);
@@ -11,5 +64,17 @@ public class MenuLogic : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit(0);
+    }
+
+    public void OptionsEnable()
+    {
+        menuButtonsParent.SetActive(false);
+        optionsButtonsParent.SetActive(true);
+    }
+
+    public void OptionsDisable()
+    {
+        menuButtonsParent.SetActive(true);
+        optionsButtonsParent.SetActive(false);
     }
 }
